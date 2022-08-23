f431e31ebc56395368e80d7aadd027f59ca390ce
Babak Arjomand <babak@Babaks-MacBook-Air.local>
2022-08-23 23:50:57 +0200

    budgetModal update

--- src/components/addBudgetModal.js
+++ src/components/addBudgetModal.js
@@ -1,26 +1,18 @@
 import { Modal, Form, Button } from "react-bootstrap";
 import { useBudgets } from "./BudgetsContext";
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 
 export default function AddBudgetModal({ show, handleClose }) {
   const nameRef = useRef();
   const maxRef = useRef();
   const { addBudget } = useBudgets();
 
-  useEffect(() => {
-    const value = nameRef.current;
-    console.log(value, "value");
-  }, []);
-
   function handleSubmit(e) {
     e.preventDefault();
     addBudget({
-      name: nameRef.current,
-      max: parseFloat(maxRef.current),
+      name: nameRef.current.value,
+      max: parseFloat(maxRef.current.value),
     });
-    console.log(nameRef.current, " manmee");
-    console.log(maxRef, "max");
-    console.log({ nameRef }, "namereff");
     handleClose();
   }
 
@@ -33,11 +25,17 @@ export default function AddBudgetModal({ show, handleClose }) {
         <Modal.Body>
           <Form.Group className="mb-3" controlId="name">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" required />
+            <Form.Control type="text" required ref={nameRef} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="max">
             <Form.Label>Maximum Spend</Form.Label>
-            <Form.Control type="number" required min={0} step={0.01} />
+            <Form.Control
+              type="number"
+              required
+              min={0}
+              step={0.01}
+              ref={maxRef}
+            />
           </Form.Group>
           <div className="d-flex justify-content-end">
             <Button variant="primary" type="submit">
